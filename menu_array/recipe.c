29a60d5964d8f923c111fc8ded36a85a663709a0
Charles A. Hessifer <hessifer@gmail.com>
2021-03-06 16:21:10 -0500

    updated for loop

--- menu_array/recipe.c
+++ menu_array/recipe.c
@@ -2,12 +2,12 @@
 
 int main(void) {
     int ingredients[10];
-    int i = 0;
     int ingredient_id = 0;
 
-    for (i=0; i < 10; i++) {
+    for (int i = 0; i < 10; i++) {
         scanf("%d", &ingredients[i]);
     }
+
     scanf("%d", &ingredient_id);
     printf("%d\n", ingredients[ingredient_id]);
 
