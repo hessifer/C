df4d3b0837595ab6af8f3989ed3d010974804144
Charles A. Hessifer <hessifer@gmail.com>
2021-03-14 21:23:47 -0400

    updated code to make it more succinct

--- sum_of_digits/sum_of_digits.c
+++ sum_of_digits/sum_of_digits.c
@@ -19,14 +19,11 @@ int sumOfDigits(int number)
     {
         return number;
     }
-    // calculate sum of all digits using recursion
     else
     {
 	// isolate right most number by using % 10
-        int rightMostNum = number % 10;
-
 	// use integer division to remove last digit and
 	// recurse to calculate the sum
-        return rightMostNum + sumOfDigits((number / 10));
+        return ((number % 10) + sumOfDigits((number / 10)));
     }
 }
