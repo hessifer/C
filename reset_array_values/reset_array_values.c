24d5a599aaa8413b87b54631a2c287c25fac1593
Charles A. Hessifer <hessifer@gmail.com>
2021-03-20 00:04:08 -0400

    updating

--- reset_array_values/reset_array_values.c
+++ reset_array_values/reset_array_values.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 void resetArrayValues(int*);
+void resetArrayValuesTwo(int []);
 
 int main(void)
 {
     // declare array of integers
     int numbers[] = {2, 4, 6, 8};
-    int *ptr = numbers;
+    //int *ptr = numbers;
     int lengthOfNumbers = sizeof(numbers) / sizeof(int);
     
     for (int i=0; i<lengthOfNumbers; i++)
     {
         printf("%d\n", numbers[i]);
     }
-    resetArrayValues(ptr);
+    //resetArrayValues(ptr);
+    resetArrayValuesTwo(numbers);
     for (int i=0; i<lengthOfNumbers; i++)
     {
         printf("%d\n", numbers[i]);
@@ -27,4 +29,12 @@ void resetArrayValues(int *ptr)
     *(ptr+1) = 0;
     *(ptr+2) = 0;
     *(ptr+3) = 0;
+}
+
+void resetArrayValuesTwo(int arr[])
+{
+    arr[0] = 0;
+    arr[1] = 0;
+    arr[2] = 0;
+    arr[3] = 0;
 }
